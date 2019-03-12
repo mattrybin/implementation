8dd5fb386370ec34013f7926c573e4906bdaf46b
mattrybin <workmateuszrybin@gmail.com>
2019-03-12 10:59:34 +0700

    Add

--- implementation/generateIdArray.js
+++ implementation/generateIdArray.js
@@ -3,5 +3,5 @@ const generateID = require('./lib/generateID')
 exports.handler = function(event, context, callback) {
     
     //callback(null, generateID(100))
-    callback(null, event)
+    callback(null, context)
 }
